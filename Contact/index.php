690968f5e8d422dae238401073d1749f7644cd67
louiemontes <louiemontes@gmail.com>
2017-04-11 02:05:23 -0700

    finish to the project 8

--- Contact/index.php
+++ Contact/index.php
@@ -11,18 +11,7 @@
   <link type="text/css" rel="stylesheet" href="../stylesheet.css"/>
   <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 </head>
-<script>
-function translateToEnglish(){
-  // just reloading the page currently
-  history.go(0);
-}
-function translateToSpanish() {
-  document.getElementById("mainContent").innerHTML = "<h1>Will translate to Spanish soon!</h1>";
-}
-function translateToArabic(){
-  document.getElementById("mainContent").innerHTML = "<h1>Will translate to Arabic later!</h1>";
-}
-</script>
+
 
 <body>
   <div id = "pageContainer">
@@ -80,33 +69,136 @@ function translateToArabic(){
     </div>
 
     <div id = "mainContent">
-      <form action="send.php" method="POST">
+
+
+
+<script>
+function translateToEnglish(){
+  // just reloading the page currently
+  history.go(0);
+}
+function translateToSpanish() {
+  document.getElementById("mainContent").innerHTML = "<h1>Will translate to Spanish soon!</h1>";
+}
+function translateToArabic(){
+  document.getElementById("mainContent").innerHTML = "<h1>Will translate to Arabic later!</h1>";
+}
+var nameOkay = false;
+var emailOkay = false;
+var subjectOkay = false;
+var bodyOkay = false;
+
+
+function checkName() {
+  var nameInput = document.getElementById("name").value;
+
+  // top answer from stackoverflow for an quick way to check an input for only numbers 
+  // borrowed from http://stackoverflow.com/questions/5778020/check-whether-an-input-string-contains-number
+  function isNumeric(n) { 
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+  if (isNumeric(nameInput)) {
+    document.getElementById("nameErr").innerHTML = "ERROR: Only numbers entered.";
+  } else if(nameInput.length === 0) {
+    document.getElementById("nameErr").innerHTML = "ERROR: No name entered.";
+  } else {
+    nameOkay = true;
+    document.getElementById("nameErr").innerHTML = "";
+  }
+  return 
+}
+
+function checkEmail() {
+  var emailInput = document.getElementById("email").value;
+  if (emailInput.includes("@") && emailInput.includes(".")) {
+    emailOkay = true;
+    document.getElementById("emailErr").innerHTML = "";
+  } else if(emailInput.length === 0) {
+    document.getElementById("emailErr").innerHTML = "ERROR: No email entered.";
+  } else {
+    document.getElementById("emailErr").innerHTML = "ERROR: Invalid email.";
+  }
+  return emailOkay;
+}
+
+function checkSubject() {
+  var subjectInput = document.getElementById("subject").value;
+  if (subjectInput.includes("<") || subjectInput.includes(">")) {
+    document.getElementById("subject").innerHTML = "ERROR: Bad character entered.";
+  } else if (subjectInput.length > 50){
+    document.getElementById("subject").innerHTML = "ERROR: Subject too long.";
+  } else if(subjectInput.length === 0) {
+    document.getElementById("subjectErr").innerHTML = "ERROR: No subject entered.";
+  } else {
+    subjectOkay = true;
+    document.getElementById("subjectErr").innerHTML = "";
+  }
+}
+
+function checkBody() {
+  var bodyInput = document.getElementById("body").value;
+  if (bodyInput.length === 0) {
+    document.getElementById("bodyErr").innerHTML = "ERROR: No body entered.";
+  } else {
+    bodyOkay = true;
+    document.getElementById("bodyErr").innerHTML = "";
+  }
+  return bodyOkay;
+}
+
+function finalValidator() {
+  // I did not know how else to trigger a radio button checker
+  if (!document.getElementById("messageTypeQuestion").checked && 
+    !document.getElementById("messageTypeComment").checked && 
+    !document.getElementById("messageTypeUrgent").checked) {
+      document.getElementById("typeErr").innerHTML = "ERROR: Nothing checked.";
+  } else {
+    typeOkay = true;
+    document.getElementById("typeErr").innerHTML = "";
+
+  }
+  
+  // see if I can use my php function
+  if (nameOkay && emailOkay && subjectOkay && bodyOkay && typeOkay === true) {
+    document.getElementById("finalizer").innerHTML ="<button type='submit'>Send!</button>";
+    // document.getElementById("finalizer").innerHTML ="fdsf";
+
+  } else {
+    document.getElementById("errorsReminder").innerHTML = "Fix the above errors first.";
+  }
+}
+</script>
+
+ <form action="send.php" method="POST">
         <h1>Say something to me!</h1>
-        Name: <input type="text" name="name">
-        <span class="error">*</span>
+
+        Name: <input type="text" name="name" size = "25" id ="name" onblur="checkName()"> <span id="nameErr" class="err" value=""></span>
         <br>
-        Your email: <input type="text" name="email">
-        <span class="error">*</span>
+        Your email: <input type="text" name="email" size = "25" id="email" onblur="checkEmail()"> <span id="emailErr" class="err" value=""></span>
         <br>
-        Subject: <input type="text" name="subject">
-        <span class="error">*</span>
+        Subject: <input type="text" name="subject" size = "25" id="subject" onblur="checkSubject()"> <span id="subjectErr" class="err" value=""></span>
         <br>
         <br>
         <br>
-        Body: <textarea name="body" rows="4" cols="50"></textarea>
-        <span class="error">*</span>
+        <span id="bodyErr" class="err" value=""></span>
+        <br>
+        Body:  <textarea name="body" rows="4" cols="50" id="body" onblur="checkBody()"></textarea>
         <br>
         <br>
         <br>
         <span style="margin-left: 10%;">Message Type:</span>
-        <p><input type="radio" name="messageType" value="Question"> Question<p>
-        <p><input type="radio" name="messageType" value="Comment"> Comment</p>
-        <p><input type="radio" name="messageType" value="Urgent"> Urgent</p>
+        <p><input type="radio" id="messageTypeQuestion" name="messageType" value="Question" onblur="finalValidator()"> Question<p>
+        <p><input type="radio"  id="messageTypeComment" name="messageType" value="Comment" onblur="finalValidator()"> Comment</p>
+        <p><input type="radio" id="messageTypeUrgent" name="messageType" value="Urgent" onblur="finalValidator()"> Urgent</p>
+        <span id="typeErr" class="err"></span>
+        <br>
         <br>
         <p><input type="checkbox" name="wantsAResponse" value="Yes"> Do you want a response?</p>
-        <input type="submit" name="submit" value="Send!">
+        <br>
+        <span id="errorsReminder" class="err" value=""></span>
+        <br>
+        <div id ="finalizer"></div>
       </form>
-    </div>
 
     <div id ="footer">
       <div id = "contactBar">
--- Contact/send.php
+++ Contact/send.php
@@ -6,60 +6,61 @@ $body = $_POST["body"];
 $messageType = $_POST["messageType"];
 $wantsAResponse = $_POST["wantsAResponse"];
 
-$nameerr = "";
-$emailerr= "";
-$subjecterr = "";
-$bodyerr="";
+// All checks and errors are taken out of this send.php for project 8 purposes
+// $nameerr = "";
+// $emailerr= "";
+// $subjecterr = "";
+// $bodyerr="";
 
-$nameCheck="";
-$emailCheck="";
-$subjectCheck="";
-$bodyCheck="";
+// $nameCheck="";
+// $emailCheck="";
+// $subjectCheck="";
+// $bodyCheck="";
 
 
 $to = "lm877@nau.edu";
 
 
 
-//name checks
-if(empty($_POST["name"])) {
-  $nameerr = "Enter a valid name.";
-} else {
-  $nameCheck = "1";
-}
+// //name checks
+// if(empty($_POST["name"])) {
+//   $nameerr = "Enter a valid name.";
+// } else {
+//   $nameCheck = "1";
+// }
 
-// email checks
-if(empty($_POST["email"])) {
-  $emailerr = "Enter an email this message can be answered to.";
-} else if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-  $emailerr = "Entered email is not a valid email, try again.";
-} else {
-  $emailCheck="1";
-}
+// // email checks
+// if(empty($_POST["email"])) {
+//   $emailerr = "Enter an email this message can be answered to.";
+// } else if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+//   $emailerr = "Entered email is not a valid email, try again.";
+// } else {
+//   $emailCheck="1";
+// }
 
-//subject checks
-if(empty($_POST["subject"])) {
-  $subjecterr = "Enter a subject.";
-} else {
-  $subjectCheck="1";
-}
+// //subject checks
+// if(empty($_POST["subject"])) {
+//   $subjecterr = "Enter a subject.";
+// } else {
+//   $subjectCheck="1";
+// }
 
-//body checks
-if(empty($_POST["body"])) {
-  $bodyerr = "Enter a body.";
-} else {
-  $bodyCheck="1";
-}
+// //body checks
+// if(empty($_POST["body"])) {
+//   $bodyerr = "Enter a body.";
+// } else {
+//   $bodyCheck="1";
+// }
 
-//wants a response checks
-if(empty($_POST["wantsAResponse"])) {
-  $wantsAResponse = "No";
-} else {
-  $wantsAResponse = "Yes";
-}
+// //wants a response checks
+// if(empty($_POST["wantsAResponse"])) {
+//   $wantsAResponse = "No";
+// } else {
+//   $wantsAResponse = "Yes";
+// }
 
 
-if($nameCheck === "1" && $emailCheck==="1" && $subjectCheck==="1" && $bodyCheck==="1") {
+// if($nameCheck === "1" && $emailCheck==="1" && $subjectCheck==="1" && $bodyCheck==="1") {
   $date = date("d/m/Y");
   $time = date("h:i:sa");
   $body = "From: " . $email . "\n" . "Name: " . $name . "\n" . "Message Type: " . $messageType . "\n" . "\n" . $body . "\n" . "\n"
@@ -67,17 +68,17 @@ if($nameCheck === "1" && $emailCheck==="1" && $subjectCheck==="1" && $bodyCheck=
   . "Server date: " . $date . "\n" . "Server time: " . $time . "\n"
   . "Wants a response: " . $wantsAResponse . "\n";
   mail($to, $subject, $body);
-  echo "Message sent! <a href='index.html'>Click here to send another email!</a>";
-} 
-else {
-  echo $nameerr;
-  echo "<br>";
-  echo $emailerr;
-  echo "<br>";
-  echo $subjecterr;
-  echo "<br>";
-  echo $bodyerr;
-  echo "<br>";
-  echo "Message not sent. <a href='index.html'>Click here to try again!</a>";
-}
+  echo "Message sent! <a href='index.php'>Click here to send another email!</a>";
+// }
+// else {
+//   echo $nameerr;
+//   echo "<br>";
+//   echo $emailerr;
+//   echo "<br>";
+//   echo $subjecterr;
+//   echo "<br>";
+//   echo $bodyerr;
+//   echo "<br>";
+//   echo "Message not sent. <a href='index.html'>Click here to try again!</a>";
+// }
 ?>
